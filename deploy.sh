9d597536d940652181e05996f58b0c8223496e9f
khanhkid <khanhkid@khanhkid>
2019-07-18 20:09:17 +0700

    update new blog

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 HOMEPATH=$(pwd)
+GITHUGOPATH=$(git remote show origin)
 
+echo ">> Generate static html"
 hugo
+echo ">> Push content of post to GitHub: $GITHUGOPATH"
 git commit -am "update new blog"
 git push origin master 
+echo ">> Pushed content of post"
+
 cd ../../
+GITHUGOPATH=$(git remote show origin)
+echo ">> Push New Content of post to GitHub: $GITHUGOPATH"
 git commit -am "upload new blog"
 git push origin master 
+echo ">> Pushed new blog"
 cd $HOMEPATH
