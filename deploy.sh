df5a80a3ebab008d090c99a3d630bc92b2d8ba70
khanhkid <khanhkid@khanhkid>
2019-07-18 20:22:31 +0700

    update new blog

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ GITHUGOPATH=$(git remote -v | grep push)
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo "${RED}>> Generate static html"
+echo "${RED}>> Generate static html${NC}"
 hugo
-echo "${RED}>> Push content of post to GitHub: $GITHUGOPATH"
+echo "${RED}>> Push content of post to GitHub: $GITHUGOPATH ${NC}"
 git add .
 git commit -am "update new blog"
 git push origin master 
@@ -14,9 +14,9 @@ echo "${RED}>> Pushed content of post"
 
 cd ../../
 GITHUGOPATH=$(git remote -v | grep push)
-echo "${RED}>> Push New Content of post to GitHub: $GITHUGOPATH"
+echo "${RED}>> Push New Content of post to GitHub: $GITHUGOPATH ${NC}"
 git add .
 git commit -am "upload new blog"
 git push origin master 
-echo "${RED}>> Pushed new blog"
+echo "${RED}>> Pushed new blog${NC}"
 cd $HOMEPATH
